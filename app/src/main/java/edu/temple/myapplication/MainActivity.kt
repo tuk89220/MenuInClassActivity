876852c411ca4e87c5a77fd4332fc6ff7d242c64
Ben-Foran <tuk89220@temple.edu>
2023-04-25 15:12:21 -0400

    added menu with actions start, pause, stop

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -62,11 +62,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return super.onOptionsItemSelected(item)
+
+        if(item.itemId == R.id.action_start){ binder?.start(100)
+            return true}
+
+        if(item.itemId == R.id.action_pause){ binder?.pause()
+            return true}
+
+        if(item.itemId == R.id.action_stop) { binder?.stop()
+            return true }
+        return false //false
     }
 
     override fun onDestroy() {
